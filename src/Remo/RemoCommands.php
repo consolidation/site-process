a7d13afd5eaba73c5fa4131ae5a55dfeb5dcd8a9
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-10-25 16:12:34 -0700

    Minor fixup for RemoCommands (not yet fully implemented though)

--- src/Remo/RemoCommands.php
+++ src/Remo/RemoCommands.php
@@ -2,17 +2,33 @@
 
 namespace Consolidation\SiteProcess\Remo;
 
+use Consolidation\SiteProcess\SiteProcess;
+
+use Consolidation\SiteAlias\SiteAliasManagerAwareInterface;
+use Consolidation\SiteAlias\SiteAliasManagerAwareTrait;
+
+
 class RemoCommands extends \Robo\Tasks
 {
+    use SiteAliasManagerAwareTrait;
+
     /**
      * Run a command identified by a site alias
      *
      * @command run
      */
-    public function run($siteAlias, array $args, $options = ['foo' => 'bar'])
+    public function run($aliasName, array $args, $options = ['foo' => 'bar'])
     {
-        $process = new \SiteProcess\SiteProcess($siteAlias, $args);
+        $this->io()->text("Not implemented yet.");
 
-        $this->io()->text("implementation tbd");
+        // The site alias manager has not been added to the DI container yet.
+        if (!$this->hasSiteAliasManager()) {
+            throw new \Exception('DI container has not been provided the alias manager yet. Implement me!');
+        }
+
+        // In theory this might do something once we get an alias manager.
+        $siteAlias = $this->siteAliasManager()->get($aliasName);
+        $process = new \SiteProcess\SiteProcess($siteAlias, $args);
+        $process->mustRun();
     }
 }
