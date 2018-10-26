af177e5de2cae6a77872e1440d389b893cbe45de
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-10-25 17:45:27 -0700

    Doxygen for the ArgumentProcessor

--- src/Util/ArgumentProcessor.php
+++ src/Util/ArgumentProcessor.php
@@ -5,8 +5,26 @@ use Consolidation\SiteAlias\AliasRecord;
 use Symfony\Component\Process\Process;
 use Consolidation\Config\Util\Interpolator;
 
+/**
+ * ArgumentProcessor takes a set of arguments and options from the caller
+ * and processes them with the provided site alias to produce a final
+ * executable command that will run either locally or on a remote system,
+ * as applicable.
+ */
 class ArgumentProcessor
 {
+    /**
+     * selectArgs selects the appropriate set of arguments for the command
+     * to be executed and orders them as needed.
+     *
+     * @param AliasRecord $siteAlias Description of
+     * @param array $args Command and arguments to execute (source)
+     * @param array $options key / value pair of option and value in include
+     *   in final arguments
+     * @param array $optionsPassedAsArgs key / value pair of option and value
+     *   to include in final arguments after the '--' argument.
+     * @return array Command and arguments to execute
+     */
     public function selectArgs(AliasRecord $siteAlias, $args, $options = [], $optionsPassedAsArgs = [])
     {
         // Split args into three arrays separated by the `--`
@@ -39,6 +57,12 @@ class ArgumentProcessor
         return $this->wrapWithTransport($siteAlias, $processedArgs);
     }
 
+    /**
+     * findArgSeparator finds the "--" argument in the provided arguments list,
+     * if present, and returns the arguments in three sets.
+     *
+     * @return array of three arrays, leading, "--" and trailing
+     */
     protected function findArgSeparator($args)
     {
         $pos = array_search('--', $args);
@@ -53,6 +77,13 @@ class ArgumentProcessor
         ];
     }
 
+    /**
+     * convertOptions takes an associative array of options (key / value) and
+     * converts it to an array of strings in the form --key=value.
+     *
+     * @param array $options in key => value form
+     * @return array options in --option=value form
+     */
     protected function convertOptions($options)
     {
         $result = [];
@@ -69,6 +100,16 @@ class ArgumentProcessor
         return $result;
     }
 
+    /**
+     * wrapWithTransport examines the provided site alias; if it is a local
+     * alias, then the provided arguments are returned unmodified. If the
+     * alias points at a remote system, though, then the arguments are
+     * escaped and wrapped in an appropriate ssh command.
+     *
+     * @param AliasRecord $siteAlias alias record of target site.
+     * @param array $args arguments provided by caller.
+     * @return array command and arguments to execute.
+     */
     protected function wrapWithTransport(AliasRecord $siteAlias, $args)
     {
         if (!$siteAlias->isRemote()) {
@@ -87,6 +128,23 @@ class ArgumentProcessor
         ];
     }
 
+    /**
+     * interpolate examines each of the arguments in the provided argument list
+     * and replaces any token found therein with the value for that key as
+     * pulled from the given site alias.
+     *
+     * Example: "git -C {{root}} status"
+     *
+     * The token "{{root}}" will be converted to a value via $siteAlias->get('root').
+     * The result will replace the token.
+     *
+     * It is possible to use dot notation in the keys to access nested elements
+     * within the site alias record.
+     *
+     * @param AliasRecord $siteAlias
+     * @param type $args
+     * @return type
+     */
     protected function interpolate(AliasRecord $siteAlias, $args)
     {
         $interpolator = new Interpolator();
