a27fb7725b2bdbe4531872571365f1fc0b8bfab7
Ivan <Chi-teck@users.noreply.github.com>
2019-04-02 21:38:38 +0500

    Do not format output (#38)

--- src/Util/RealtimeOutputHandler.php
+++ src/Util/RealtimeOutputHandler.php
@@ -102,9 +102,9 @@ class RealtimeOutputHandler
     public function handleOutput($type, $buffer)
     {
         if (Process::ERR === $type) {
-            $this->stderr->write($this->addMarker($buffer, $this->stderrMarker));
+            $this->stderr->write($this->addMarker($buffer, $this->stderrMarker), false, OutputInterface::OUTPUT_RAW);
         } else {
-            $this->stdout->write($this->addMarker($buffer, $this->stdoutMarker));
+            $this->stdout->write($this->addMarker($buffer, $this->stdoutMarker), false, OutputInterface::OUTPUT_RAW);
         }
     }
 
