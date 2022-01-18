a717f9f59610355e67b6c707ef91db3bf3ddd48c
alexpott <alex.a.pott@gmail.com>
2022-01-18 17:37:37 +0000

    Relax constraints (#58)
    * Relax symfony constraints
    
    * Use reflection to get around Symfony's method removal
    
    * Symfony 6 is going to require a new major version due to return typehints
    
    * A slightly better way
    
    * Update the lock hash
    
    * coding standards

--- src/SiteProcess.php
+++ src/SiteProcess.php
@@ -11,6 +11,7 @@ use Consolidation\Config\Util\Interpolator;
 use Consolidation\SiteProcess\Util\Shell;
 use Consolidation\SiteProcess\Util\ShellOperatorInterface;
 use Consolidation\SiteProcess\Util\Escape;
+use Symfony\Component\Process\Process;
 
 /**
  * A wrapper around Symfony Process that uses site aliases
@@ -188,7 +189,7 @@ class SiteProcess extends ProcessBase
             $processedArgs = $this->processArgs();
             $commandLine = Escape::argsForSite($this->siteAlias, $processedArgs);
             $commandLine = implode(' ', $commandLine);
-            $this->setCommandLine($commandLine);
+            $this->overrideCommandLine($commandLine);
         }
         return $commandLine;
     }
@@ -240,5 +241,23 @@ class SiteProcess extends ProcessBase
             },
             $args
         );
+    }
+
+    /**
+     * Overrides the command line to be executed.
+     *
+     * @param string|array $commandline The command to execute
+     *
+     * @return $this
+     *
+     * @todo refactor library so this hack to get around changes in
+     *   symfony/process 5 is unnecessary.
+     */
+    private function overrideCommandLine($commandline)
+    {
+        $property = new \ReflectionProperty(Process::class, "commandline");
+        $property->setAccessible(true);
+        $property->setValue($this, $commandline);
+        return $this;
     }
 }
