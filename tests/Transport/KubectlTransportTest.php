45ca343e66f5cfdc6c4397dcc7c1a1125e0cb212
Cees Geene <cees@swis.nl>
2022-11-30 08:46:51 +0100

    Allow to set an entrypoint on KubectlTransport

--- tests/Transport/KubectlTransportTest.php
+++ tests/Transport/KubectlTransportTest.php
@@ -68,6 +68,38 @@ class KubectlTransportTest extends TestCase
                     ]
                 ],
             ],
+
+            // With entrypoint as string.
+            [
+                'kubectl --namespace=vv exec --tty=false --stdin=false deploy/drupal --container=drupal -- /docker-entrypoint ls',
+                ['ls'],
+                [
+                    'kubectl' => [
+                        'tty' => false,
+                        'interactive' => false,
+                        'namespace' => 'vv',
+                        'resource' => 'deploy/drupal',
+                        'container' => 'drupal',
+                        'entrypoint' => '/docker-entrypoint',
+                    ]
+                ],
+            ],
+
+            // With entrypoint as array.
+            [
+                'kubectl --namespace=vv exec --tty=false --stdin=false deploy/drupal --container=drupal -- /docker-entrypoint --debug ls',
+                ['ls'],
+                [
+                    'kubectl' => [
+                        'tty' => false,
+                        'interactive' => false,
+                        'namespace' => 'vv',
+                        'resource' => 'deploy/drupal',
+                        'container' => 'drupal',
+                        'entrypoint' => ['/docker-entrypoint', '--debug'],
+                    ]
+                ],
+            ],
         ];
     }
 
