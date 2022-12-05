ce574f9a37f11de1c6e6e9eda8d23243858062df
Cees Geene <cees@swis.nl>
2022-12-05 16:50:34 +0100

    Fix KubectlTransport::addChdir()

--- src/Transport/KubectlTransport.php
+++ src/Transport/KubectlTransport.php
@@ -4,6 +4,7 @@ namespace Consolidation\SiteProcess\Transport;
 
 use Consolidation\SiteProcess\SiteProcess;
 use Consolidation\SiteAlias\SiteAliasInterface;
+use Consolidation\SiteProcess\Util\Escape;
 use Consolidation\SiteProcess\Util\Shell;
 
 /**
@@ -14,6 +15,9 @@ class KubectlTransport implements TransportInterface
 {
     /** @var bool */
     protected $tty;
+
+    /** @var string */
+    protected $cd_remote;
 
     /** @var \Consolidation\SiteAlias\SiteAliasInterface */
     protected $siteAlias;
@@ -64,6 +68,21 @@ class KubectlTransport implements TransportInterface
             $transport = is_array($entrypoint) ? [...$transport, ...$entrypoint] : [...$transport, $entrypoint];
         }
 
+        if ($this->cd_remote) {
+            // Wrap the command in a subshell, to be able to prepend a `cd`.
+            $args = [
+                'sh',
+                '-c',
+                // Escape each argument for the target system and then join.
+                implode(' ', Escape::argsForSite($this->siteAlias, [
+                    'cd',
+                    $this->cd_remote,
+                    Shell::op('&&'),
+                    ...$args
+                ]))
+            ];
+        }
+
         return array_merge($transport, $args);
     }
 
@@ -72,13 +91,13 @@ class KubectlTransport implements TransportInterface
      */
     public function addChdir($cd_remote, $args)
     {
-        return array_merge(
-            [
-                'cd',
-                $cd_remote,
-                Shell::op('&&'),
-            ],
-            $args
-        );
+        // If the site alias specifies a root, and it matches the requested
+        // directory, there is no need to wrap the command in a subshell.
+        if ($cd_remote === $this->siteAlias->get('root') && $this->siteAlias->get('kubectl.cd_root') === false) {
+            return $args;
+        }
+
+        $this->cd_remote = $cd_remote;
+        return $args;
     }
 }
