a873e741efcbc4297ed336ec533c3ca294c709a9
Greg Anderson <greg@pantheon.io>
2025-11-14 11:14:39 -0800

    Revert "Fix KubectlTransport::addChdir() (#72)"
    This reverts commit ddcacfd7b9386781d2dd8715db09a9ff73d522ec.

--- src/Transport/KubectlTransport.php
+++ src/Transport/KubectlTransport.php
@@ -4,7 +4,6 @@ namespace Consolidation\SiteProcess\Transport;
 
 use Consolidation\SiteProcess\SiteProcess;
 use Consolidation\SiteAlias\SiteAliasInterface;
-use Consolidation\SiteProcess\Util\Escape;
 use Consolidation\SiteProcess\Util\Shell;
 
 /**
@@ -15,9 +14,6 @@ class KubectlTransport implements TransportInterface
 {
     /** @var bool */
     protected $tty;
-
-    /** @var string */
-    protected $cd_remote;
 
     /** @var \Consolidation\SiteAlias\SiteAliasInterface */
     protected $siteAlias;
@@ -68,21 +64,6 @@ class KubectlTransport implements TransportInterface
             $transport = is_array($entrypoint) ? [...$transport, ...$entrypoint] : [...$transport, $entrypoint];
         }
 
-        if ($this->cd_remote) {
-            // Wrap the command in a subshell, to be able to prepend a `cd`.
-            $args = [
-                'sh',
-                '-c',
-                // Escape each argument for the target system and then join.
-                implode(' ', Escape::argsForSite($this->siteAlias, [
-                    'cd',
-                    $this->cd_remote,
-                    Shell::op('&&'),
-                    ...$args
-                ]))
-            ];
-        }
-
         return array_merge($transport, $args);
     }
 
@@ -91,13 +72,13 @@ class KubectlTransport implements TransportInterface
      */
     public function addChdir($cd_remote, $args)
     {
-        // If the site alias specifies a root, and it matches the requested
-        // directory, there is no need to wrap the command in a subshell.
-        if ($cd_remote === $this->siteAlias->get('root') && $this->siteAlias->get('kubectl.cd_root') === false) {
-            return $args;
-        }
-
-        $this->cd_remote = $cd_remote;
-        return $args;
+        return array_merge(
+            [
+                'cd',
+                $cd_remote,
+                Shell::op('&&'),
+            ],
+            $args
+        );
     }
 }
